e47b7fe4ba409cd05bf83382637f12d5fd7cee4a
Junior Solis <juniorjsolis@gmail.com>
2026-03-12 22:59:13 -0600

    add architecture clean and new entities in domain

---
+++ src/domain/entities/perfil.ts
@@ -0,0 +1,14 @@
+export interface SocialLink {
+nombre : string ;
+url : string ;
+
+}
+export interface Perfil {
+nombre : string ;
+localizacion : string ;
+bio : string ;
+avatar : string ;
+links: SocialLink[];
+
+}
+
